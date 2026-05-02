8bdfefc48bb530f5fe9e0651ae91b47163771151
Mikeclapz575 <michaelchaffin959@gmail.com>
2026-05-01 23:20:45 -0400

    revert wisp routing fix

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ Object.assign(wisp.options, {
 server.on("upgrade", (req, sock, head) =>
   bare?.shouldRoute(req)
     ? bare.routeUpgrade(req, sock, head)
-    : req.url.startsWith("/wisp")
+    : req.url.endsWith("/wisp/")
       ? wisp.routeRequest(req, sock, head)
       : sock.end()
 );
